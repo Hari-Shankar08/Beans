946b1383e5ed2d33b038464491291fc519d36b65
Hari-Shankar08 <65345441+Hari-Shankar08@users.noreply.github.com>
2020-11-23 10:26:48 +0530

    Add files via upload

--- bot_classesandsuch.py
+++ bot_classesandsuch.py
@@ -1,4 +1,5 @@
 from discord.ext.commands import Bot
+import discord
 import asyncio
 import pickle
 import random
@@ -23,6 +24,10 @@ guild_premium = []
 
 
 class NoPremiumError(Exception):
+    pass
+
+
+class InvalidMovement(Exception):
     pass
 
 
@@ -104,7 +109,7 @@ async def join(ctx, *args):
                     available_games[args[0]] = game_
                     games[g_id] = available_games
 
-                    in_a_game[ctx.message.author] = args[0]
+                    in_a_game[ctx.message.author] = [g_id, args[0]]
                 else:
                     await ctx.send('Sorry, that game is in progress.')
             else:
@@ -118,7 +123,7 @@ async def join(ctx, *args):
 @bot.command()
 async def which(ctx):
     if ctx.message.author in in_a_game:
-        await ctx.send(f'You have joined the game with unique code: {in_a_game[ctx.message.author]}')
+        await ctx.send(f'You have joined the game with unique code: {in_a_game[ctx.message.author][1]}')
     else:
         await ctx.send(f'You aren\'t in a game yet, {ctx.message.author}.')
 
@@ -133,18 +138,18 @@ async def nick(ctx, *args):
             if nick_.startswith('http') or nick_.strip().lower() == 'skip':
                 raise ValueError('Disallowed nickname.')
 
-            game_ = games[g_id][in_a_game[ctx.message.author]]
-            if not game_.game_in_progress and nick_ not in game_.player_colours.values():
-                game_.player_colours[ctx.message.author] = nick
+            gm = games[g_id][in_a_game[ctx.message.author]]
+            if not gm.game_in_progress and nick_ not in game_.player_colours.values():
+                gm.player_colours[ctx.message.author] = nick
                 await ctx.send('Ok, nickname set.')
-                print(len(game_.players) == len(game_.player_colours) == game_.n_players)
-                if len(game_.players) == len(game_.player_colours) == game_.n_players:
+                print(len(gm.players) == len(game_.player_colours) == game_.n_players)
+                if len(gm.players) == len(game_.player_colours) == game_.n_players:
                     await ctx.send('Alright, starting game.\n\nNo this is not done yet.')
                     await start_game(game_)
-                games[g_id][in_a_game[ctx.message.author]] = game_
-            elif game_.game_in_progress:
+                games[g_id][in_a_game[ctx.message.author]] = gm
+            elif gm.game_in_progress:
                 await ctx.semd('Hey! You can\'t change nicknames in between the game!')
-            elif nick_ in game.player_colours.values():
+            elif nick_ in gm.player_colours.values():
                 raise ValueError('Nickname has already been taken.')
         else:
             await ctx.send('You aren\'t in a game yet, {}'.format(ctx.message.author))
@@ -156,9 +161,84 @@ async def nick(ctx, *args):
         await ctx.send(f'I can\'t allow you to take that nickname.\n```\nReason: {str(ve)}```')
 
 
-async def start_game(game):
+@bot.command()
+async def m(ctx, tile):
+    try:
+        if ctx.message.author in in_a_game:
+            tile = int(tile)
+            g_id, u_id = tuple(in_a_game[ctx.message.author])
+            gm = games[g_id][u_id]
+            await move_player(gm, pl, tile)
+            await ctx.send('Movement not implemented yet.')
+    except ValueError:
+        await ctx.send('That\'s not allowed. Use "!map" to see a map of allowed tiles.')
+
+# --- Non bot commands ---
+
+async def start_game(gm):
+    for player in gm.players:
+        await player.send("Game is now in progress.")
+    gm.game_in_progress = True
+    await asyncio.sleep(2)
+    gm.players_alive = [1] * len(players)  # 1 for alive, 0 for dead
+    gm.player_locations = [14] * len(players_alive)
+    gm.impostors = random.sample(players, n_impostors)
+    gm.has_killed = dict.fromkeys(impostors, True)
+    d = {player: player_colours[player] for player in players}
+    for i in range(len(gm.players)):
+        if gm.players[i] in impostors:
+            await gm.players[i].send('You are the `impostor`. Don\'t get caught!')
+            embed = await make_embed('In Game', 'Players and their Nicknames:', dict_=d, is_impostor=True, gm=gm)
+            await gm.players[i].send(embed=embed)
+        else:
+            await gm.players[i].send(f'You are a `crew-mate`. There is {len(impostors)} impostor among you.')
+            embed = await make_embed('In Game', 'Players and their Nicknames:', dict_=gm.player_colours)
+            await gm.players[i].send(embed=embed)
+
+        await gm.players[i].send(f"You are currently in {skeld_locations[player_locations[i]]} \
+(Tile {player_locations[i]})")
+        await asyncio.sleep(15)
+        gm.has_killed = dict.fromkeys(impostors, False)
+        gm.can_emergency = True
+
+        await putback_game(gm, random.choice(gm.players))
+
+
+async def putback_game(gm, pl):
+    global games
+    global in_a_game
+
+    g_id, u_id = tuple(in_a_game(pl))
+    games[g_id][u_id] = gm
+    print('Games dictionary updated.')
+
+
+async def move_player(gm, pl, tile):
+    if pl in gm.players:
     pass
 
+async def make_embed(title, desc, dict_, is_impostor=False, footer=None, gm=None):
+    embed = discord.Embed(title=title,
+                          description=desc)
+    embed.set_thumbnail(url=bot.user.avatar_url)
+    if not is_impostor:
+        for key, val in dict_.items():
+            embed.add_field(name=str(key), value=val)
+    else:
+        for key, val in dict_.items():
+            if key in gm.impostors:
+                embed.add_field(name=str(key) + ' (Impostor)', value=val)
+            else:
+                embed.add_field(name=str(key), value=val)
+    if footer is not None:
+        embed.set_footer(text=footer, icon_url=bot.user.avatar_url)
+    else:
+        embed.set_footer(text='BEAN SUS: TEXT BASED AMONG US ON DISCORD', icon_url=bot.user.avatar_url)
+
+    return embed
+
+
+# --- Running the bot ---
 
 if __name__ == '__main__':
     print('Running...')
